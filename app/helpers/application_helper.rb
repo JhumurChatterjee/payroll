0c9af491d6911da5cfcc7e795e72018de1d7bf13
Jhumur Chatterjee <jhumurchatterjee1996@gmail.com>
2019-10-13 01:14:48 +0530

    Adding flash message for this application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,19 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def flash_message_prefix(message_type)
+    case message_type
+    when "success"
+      "Well done!"
+    when "info"
+      "Heads up!"
+    when "warning"
+      "Warning!"
+    when "danger"
+      "Oh snap!"
+    else
+      ""
+    end
+  end
 end
