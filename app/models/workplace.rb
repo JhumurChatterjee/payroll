21f9de7ed43898a7f842f56f7dee007dd270791a
Jhumur Chatterjee <jhumurchatterjee1996@gmail.com>
2019-10-14 20:55:58 +0530

    Decreasing minimum length of workplace name

--- app/models/workplace.rb
+++ app/models/workplace.rb
@@ -2,7 +2,7 @@ class Workplace < ApplicationRecord
   before_validation :format_name
   after_create :create_tenant
 
-  validates :name, presence: true, length: { minimum: 6, maximum: 50 }, uniqueness: { case_sensitive: false },
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }, uniqueness: { case_sensitive: false },
                    format: { with: /\A[-a-z0-9]+\z/, message: "only allows letters, hyphen & numbers" }
 
   private
